56f660171757f4e692a72d29effc3335a50f3ead
Yusuf Soomro <yusuf.soomro@omadahealth.com>
2016-12-23 01:22:59 -0800

    oh god it's so janky

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,12 +3,15 @@ class TasksController < ActionController::Base
     @most_recent_task = TaskList.last
     @most_recent_task.update!(times_displayed: @most_recent_task.times_displayed + 1) if !Rails.env.development?
 
+    nightbot_url = request.env["HTTP_NIGHTBOT_RESPONSE_URL"]
+    nightbot_url[4] = ''
+
     Rails.logger.info(params.keys)
     Rails.logger.info(params.values)
     Rails.logger.info(request.env.inspect)
-    Rails.logger.info(request.env["HTTP_NIGHTBOT_RESPONSE_URL"])
+    Rails.logger.info(nightbot_url)
 
-    TaskListPoster.perform_later(request.env["HTTP_NIGHTBOT_RESPONSE_URL"])
+    TaskListPoster.perform_later(nightbot_url)
     head :ok
   end
 end
