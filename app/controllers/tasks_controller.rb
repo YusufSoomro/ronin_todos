19cf828fdbb5077f2913b2b552c16b74a838defd
Yusuf Soomro <yusuf.soomro@omadahealth.com>
2016-12-23 00:20:22 -0800

    Trying to debug

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,9 @@ class TasksController < ActionController::Base
   def index
     @most_recent_task = TaskList.last
     @most_recent_task.update!(times_displayed: @most_recent_task.times_displayed + 1) if !Rails.env.development?
+
+    Rails.logger.info(params.keys)
+    Rails.logger.info(params.values)
     render "discord_task_template.txt.erb", content_type: "text/plain"
   end
 end
