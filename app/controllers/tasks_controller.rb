35cf7092de58a2477a66e27b94a9414259b43729
Yusuf Soomro <yusuf.soomro@omadahealth.com>
2016-12-23 16:49:44 -0800

    Got the content_type wording mixed up

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,12 +3,12 @@ class TasksController < ActionController::Base
     @most_recent_task_list = TaskList.last
     @first_unfinished_task = @most_recent_task_list.tasks.detect { |task| !task.is_really_done? }
 
-    render "discord_task_template.txt.erb", content_type: "plain/text"
+    render "discord_task_template.txt.erb", content_type: "text/plain"
   end
 
   def all_tasks
     @most_recent_task_list = TaskList.last
 
-    render "all_tasks.txt.erb", content_type: "plain/text"
+    render "all_tasks.txt.erb", content_type: "text/plain"
   end
 end
